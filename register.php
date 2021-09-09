c2421b20ba2dbdefdb7bb39b8299de39eb9401a0
Rashida v <rashidav27@gmail.com>
2021-09-09 22:21:56 +0530

    update

--- register.php
+++ register.php
@@ -8,20 +8,16 @@
     $dbconn = pg_connect($connection_string) or die('Could not connect: ' . pg_last_error()) ;
 
 if(isset($_POST['submit'])&&!empty($_POST['submit'])){
-    
-    $hashpassword = md5($_POST['password']);
-
-     $sql ="select *from public.users where email = '".pg_escape_string($_POST['user'])."' and password ='".$hashpassword."'";
   
-    $data = pg_query($dbconn,$sql); 
-    $login_check = pg_num_rows($data);
-    if($login_check > 0){ 
+    $sql = "insert into public.users(email,password,username)values('".$_POST['email']."','".md5($_POST['password'])."','".$_POST['user']."')";
+    $ret = pg_query($dbconn, $sql);
+    if($ret){
         
-        header('Location: index.html');    
-       
+        header('Location: index.html');
     }else{
+            echo $sql;
         
-        echo "Invalid Details";
+          
     }
 }
 
